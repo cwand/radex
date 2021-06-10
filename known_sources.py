b3266301b4489114543d97d93621548998934a4c
Christian Walther Andersen <christian.walther.andersen@rsyd.dk>
2021-06-10 09:51:53 +0200

    Uncertainty on calibratio ignored with only one calibration source

--- known_sources.py
+++ known_sources.py
@@ -8,42 +8,47 @@ from pathlib import Path
 # Get spectrum data from sources with known activity and measure sensitivity
 def sensitivity():
 
-  # Read configuration to find calibration data
-  config = configparser.ConfigParser()
-  config.read('config.ini')
+	# Read configuration to find calibration data
+	config = configparser.ConfigParser()
+	config.read('config.ini')
 
-  # Create known source index from index file
-  ks_index = {}
-  with open(config['calib']['calfiles']+'src.txt') as f:
-    lines = [line.rstrip() for line in f]
-  for s in lines:
-    cont = s.split(';')
-    ks_index[cont[0]] = int(cont[1])
+	# Create known source index from index file
+	ks_index = {}
+	with open(config['calib']['calfiles']+'src.txt') as f:
+		lines = [line.rstrip() for line in f]
+	for s in lines:
+		cont = s.split(';')
+		ks_index[cont[0]] = int(cont[1])
 
-  sens_obs = []
-  for src in ks_index:
+	sens_obs = []
+	for src in ks_index:
 
-  	# Load spectrum from file
-  	src_spec = spectrum.load_from_file(config['calib']['calfiles']+src+'.txt')
+		# Load spectrum from file
+		src_spec = spectrum.load_from_file(config['calib']['calfiles']+src+'.txt')
 
-  	# Calculate sensitivity (cps/Bq) from known source
+		# Calculate sensitivity (cps/Bq) from known source
 
-  	#	Get rate in windows
-  	r_s = 0			# Net source rate
-  	for window in physics.windows['Ra223']:
-  		r_s += src_spec.window_rate(window)
+		#	Get rate in windows
+		r_s = 0			# Net source rate
+		for window in physics.windows['Ra223']:
+			r_s += src_spec.window_rate(window)
 
-  	# Get known activity
-  	src_act = ks_index[src]
+		# Get known activity
+		src_act = ks_index[src]
 
-  	# Calculate sensitivity estimate from this source
-  	sens_obs.append(r_s/src_act)		# Sensitivity [cps/Bq]
+		# Calculate sensitivity estimate from this source
+		sens_obs.append(r_s/src_act)		# Sensitivity [cps/Bq]
 
-  # Calculate mean sensivity and uncertainty on sensitivity
-  sens_obs = np.array(sens_obs)
-  sens = np.mean(sens_obs)
-  dsens = np.std(sens_obs, ddof=1)/math.sqrt(sens_obs.size)
-  return (sens,dsens)
+	# Calculate mean sensivity and uncertainty on sensitivity
+	sens_obs = np.array(sens_obs)
+	sens = np.mean(sens_obs)
+	if sens_obs.size == 1:
+		print('Usikkerhed på følsomhed bliver ignoreret, '
+					'da der kun er registreret 1 kalibrering!')
+		dsens = 0
+	else:
+		dsens = np.std(sens_obs, ddof=1)/math.sqrt(sens_obs.size)
+	return (sens,dsens)
 
 
 def write_calibration(spectrum, activity, name):
